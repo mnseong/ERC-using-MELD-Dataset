519e10942082ae20d15e25a837bece8e475f643c
mnseong <mskweon@icloud.com>
2023-03-08 11:47:41 +0900

    Feat dataset.py

--- dataset.py
+++ dataset.py
@@ -1,5 +1,8 @@
+from torch.utils.data import Dataset
+from transformers import RobertaTokenizer
 import csv
 from torch.utils.data import Dataset
+import torch
 
 
 def split(session):
@@ -15,7 +18,9 @@ class CustomDataLoader(Dataset):
     def __init__(self, data_path):
         f = open(data_path, 'r')
         rdr = csv.reader(f)
+        
         emo_set = set()
+        self.tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
 
         self.session_dataset = []
         session = []
@@ -58,14 +63,47 @@ class CustomDataLoader(Dataset):
 
     def __getitem__(self, idx):
         return self.session_dataset[idx]
+    
+    def padding(self, batch_input_token):
+        batch_token_ids, batch_attention_masks = batch_input_token['input_ids'], batch_input_token['attention_mask']
+        trunc_batch_token_ids, trunc_batch_attention_masks = [], []
+        for batch_token_id, batch_attention_mask in zip(batch_token_ids, batch_attention_masks):
+            if len(batch_token_id) > self.tokenizer.model_max_length:
+                trunc_batch_token_id = [batch_token_id[0]] + batch_token_id[1:][-self.tokenizer.model_max_length+1:]
+                trunc_batch_attention_mask = [batch_attention_mask[0]] + batch_attention_mask[1:][-self.tokenizer.model_max_length+1:]
+                trunc_batch_token_ids.append(trunc_batch_token_id)
+                trunc_batch_attention_masks.append(trunc_batch_attention_mask)
+            else:
+                trunc_batch_token_ids.append(batch_token_id)
+                trunc_batch_attention_masks.append(batch_attention_mask)
+        
+        max_length = max([len(x) for x in trunc_batch_token_ids])
+        padding_tokens, padding_attention_masks = [], []
+        for batch_token_id, batch_attention_mask in zip(trunc_batch_token_ids, trunc_batch_attention_masks):
+            padding_tokens.append(batch_token_id + [self.tokenizer.pad_token_id for _ in range(max_length-len(batch_token_id))])
+            padding_attention_masks.append(batch_attention_mask + [0 for _ in range(max_length-len(batch_token_id))]                                                        )
+        return torch.tensor(padding_tokens), torch.tensor(padding_attention_masks)
 
     def collate_fn(self, sessions):
-        batch_input_token = []
+        batch_input, batch_labels = [], []
+        batch_PM_input = []
         for session in sessions:
-            input_token = ""
-            for line in session:
+            input_str = self.tokenizer.cls_token
+            
+            # PM
+            current_speaker, current_utt, current_emotion = session[-1]
+            PM_input = []
+            for i, line in enumerate(session):
                 speaker, utt, emotion = line
-                input_token += utt
-            batch_input_token.append(input_token)
-
-        return batch_input_token
+                input_str += " " + utt + self.tokenizer.sep_token
+                if i < len(session)-1 and current_speaker == speaker:
+                    PM_input.append(self.tokenizer.encode(utt, add_special_tokens=True, return_tensors='pt'))
+                    
+            # CoM
+            batch_input.append(input_str)
+            batch_labels.append(self.emoList.index(emotion))
+            batch_PM_input.append(PM_input)
+        batch_input_token = self.tokenizer(batch_input, add_special_tokens=False)
+        batch_padding_token, batch_padding_attention_mask = self.padding(batch_input_token)
+        
+        return batch_padding_token, batch_padding_attention_mask, batch_PM_input, torch.tensor(batch_labels)
